a5e16b80fe43aa5662f0e4f96b771f5586865cb0
erkezhan <erkezhanasubaeva@gmail.com>
2024-01-09 23:22:39 +0600

    edit

--- practice19/app/script.js
+++ practice19/app/script.js
@@ -3,27 +3,27 @@ function showMessageCallback(message) {
   document.getElementById('message').innerText = message;
 }
 
-// Функция, использующая таймаут для вызова колбэка через 3 секунды
+// функция использующая таймаут для вызова колбэка через 3 секунды
 function showDelayedMessage() {
   setTimeout(function () {
       showMessageCallback('Прошло 3 секунды!');
   }, 3000);
 }
 
-// Функция, которая будет вызываться с интервалом каждые 5 секунд
+// функция которая будет вызываться с интервалом каждые 5 секунд
 function updateMessageInterval() {
   setInterval(function () {
       showMessageCallback('Обновлено каждые 5 секунд!');
   }, 5000);
 }
 
-// Запуск функции с задержкой
+// запуск функции с задержкой
 showDelayedMessage();
 
-// Запуск функции с интервалом
+//запуск функции с интервалом
 updateMessageInterval();
 
-// Функция, которая будет вызываться с интервалом для создания анимации
+// вызывается с интервалом для создания анимации
 function animateElement() {
   let rotation = 0;
   const animatedElement = document.getElementById('animatedElement');
@@ -34,5 +34,4 @@ function animateElement() {
   }, 1000);
 }
 
-// Запуск функции для создания анимации
 animateElement();

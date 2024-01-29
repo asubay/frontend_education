f4c3960fb7c319e962037777a887aacfb92f0189
erkezhan.asubaeva <erkezhanasubaeva@gmail.com>
2024-01-29 11:00:27 +0600

    hw24-25

--- react_24/my_react/src/App.js
+++ react_24/my_react/src/App.js
@@ -1,13 +1,20 @@
 import MyButton from "./Components/UI/buttons/MyButton";
 import classes from './Components/Styles/App.module.css'
 import CurrentWeather from "./Components/UI/info/CurrentWeather";
-
+import React, { useState } from 'react';
 function App() {
+    const [currentDate, setCurrentDate] = useState(null);
+
+    const handleButtonClick = () => {
+        const now = new Date();
+        setCurrentDate(now.toLocaleString());
+    };
        return (
         <div className="App">
             <div className={classes.container}>
-                <CurrentWeather/>
-                <MyButton>текущая дата</MyButton>
+                <MyButton onClick={handleButtonClick} text={'текущая дата'}></MyButton>
+                {currentDate && <p>Сейчас в городе Усть-Каменогорск: {currentDate}</p>}
+                <CurrentWeather/>br
             </div>
         </div>
     );
--- react_24/my_react/src/Components/UI/buttons/MyButton.jsx
+++ react_24/my_react/src/Components/UI/buttons/MyButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import classes from './MyButton.module.css'
 
-const MyButton = ({children, ...props}) => {
+const MyButton = (props) => {
     return (
-        <button className={classes.myButton}>{children}</button>
+        <button className={classes.myButton} onClick={props.onClick}>{props.text}</button>
     );
 };
 

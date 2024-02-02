54d060c76572f9e3a556a5e8d8f33c4c93c99fbc
erkezhan.asubaeva <erkezhanasubaeva@gmail.com>
2024-02-02 19:06:17 +0600

    edit

--- react_27/my_react/src/App.js
+++ react_27/my_react/src/App.js
@@ -1,22 +1,29 @@
 import CityWeather from "./Components/UI/info/CityWeather";
 import Container from "./Components/UI/info/Container";
+
+const cities = [{
+    cityName: 'Усть-Каменогорск',
+    date: '01.02.2024',
+    humidity: 94,
+    temperature: '-15',
+    feels_like: '-17',
+    pressure: '735'
+},{
+    cityName: 'Усть-Павлодар',
+    date: '01.02.2024',
+    humidity: 87,
+    temperature: '-13',
+    feels_like: '-14',
+    pressure: '745'
+}]
+
 export default function Profile() {
+
     return (
         <Container>
-            <CityWeather cityName={'Усть-Каменогорск'}
-                         date={'01.02.2024'}
-                         humidity={94}
-                         temperature={'-15'}
-                         feels_like={'-17'}
-                         pressure={'745'}
-            />
-            <CityWeather cityName={'Павлодар'}
-                         date={'01.02.2024'}
-                         humidity={71}
-                         temperature={'-10'}
-                         feels_like={'-11'}
-                         pressure={'735'}
-            />
+            {
+                cities.map(item => <CityWeather {...item}/>)
+            }
         </Container>
     );
 }

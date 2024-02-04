429faa5df4a4280089282f17f4acc3b6a51192c2
erkezhan <erkezhanasubaeva@gmail.com>
2024-02-04 21:45:18 +0600

    fix key index

--- react_27/my_react/src/App.js
+++ react_27/my_react/src/App.js
@@ -2,27 +2,38 @@ import CityWeather from "./Components/UI/info/CityWeather";
 import Container from "./Components/UI/info/Container";
 
 const cities = [{
+    id: 1520316,
     cityName: 'Усть-Каменогорск',
     date: '01.02.2024',
     humidity: 94,
     temperature: '-15',
     feels_like: '-17',
     pressure: '735'
-},{
-    cityName: 'Усть-Павлодар',
+}, {
+    id: 1520240,
+    cityName: 'Павлодар',
     date: '01.02.2024',
     humidity: 87,
     temperature: '-13',
     feels_like: '-14',
     pressure: '745'
-}]
+},
+    {
+        id: 1519422,
+        cityName: 'Семей',
+        date: '01.02.2024',
+        humidity: 67,
+        temperature: '-6',
+        feels_like: '-6',
+        pressure: '749'
+    }]
 
-export default function Profile() {
+export default function CityInfo() {
 
     return (
         <Container>
             {
-                cities.map(item => <CityWeather {...item}/>)
+                cities.map((item) => <CityWeather key={item.id} {...item} />)
             }
         </Container>
     );
